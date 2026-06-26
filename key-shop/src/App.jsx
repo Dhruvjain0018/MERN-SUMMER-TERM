723e4f3879ce95d4a14c1a715c4f71b793d7cc15
Jain0188 <dhruvjain0018@gmail.com>
2026-06-26 19:07:34 +0530

    Initial commit

---
+++ key-shop/src/App.jsx
@@ -0,0 +1,56 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import "./App.css";
+
+function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  function addToCart(product) {
+    setCartItems((previousCartItems) => {
+      if(!product){
+        return;
+      }
+      const productAlreadyInCart = previousCartItems.find(
+        (item) => item.id == product.id,
+      );
+
+      if (productAlreadyInCart) {
+        return previousCartItems.map((item) => {
+          return item.id == product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item;
+        });
+      }
+      return [...previousCartItems, { ...product, quantity: 1 }];
+    });
+  }
+
+  const cartCount = cartItems.reduce((total, item) => total + item.quantity, 0);
+
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Header cartCount={cartCount} />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home addToCart={addToCart} />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact-us" element={<Contact />} />
+            <Route path="/cart" element={<Cart cartItems={cartItems} />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
